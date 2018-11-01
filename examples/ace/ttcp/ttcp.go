c234f89279141dde366bbcc34ae16508a91ccc5c
wanmin <wanminny@163.com>
2018-11-01 17:01:15 +0800

    测试

--- examples/ace/ttcp/ttcp.go
+++ examples/ace/ttcp/ttcp.go
@@ -16,8 +16,10 @@ type options struct {
 	port     int
 	length   int
 	number   int
+
 	transmit bool
 	receive  bool
+
 	nodelay  bool
 	host     string
 }
@@ -30,13 +32,18 @@ type SessionMessage struct {
 
 func init() {
 	flag.IntVar(&opt.port, "p", 5001, "TCP port")
+
 	flag.IntVar(&opt.length, "l", 65536, "Buffer length")
 	flag.IntVar(&opt.number, "n", 8192, "Number of buffers")
-	flag.BoolVar(&opt.receive, "r", false, "Receive")
-	flag.StringVar(&opt.host, "t", "", "Transmit")
+
+	flag.BoolVar(&opt.receive, "r", false, "Receive") //接受 作为服务器端
+	// 主机
+	flag.StringVar(&opt.host, "host", "", "Transmit")
+
 	muduo.Check(binary.Size(SessionMessage{}) == 8, "packed struct")
 }
 
+// 客户端发送
 func transmit() {
 	sessionMessage := SessionMessage{int32(opt.number), int32(opt.length)}
 	fmt.Printf("buffer length = %d\nnumber of buffers = %d\n",
@@ -44,6 +51,7 @@ func transmit() {
 	total_mb := float64(sessionMessage.Number) * float64(sessionMessage.Length) / 1024.0 / 1024.0
 	fmt.Printf("%.3f MiB in total\n", total_mb)
 
+	//客户端 拨号
 	conn, err := net.Dial("tcp", net.JoinHostPort(opt.host, strconv.Itoa(opt.port)))
 	muduo.PanicOnError(err)
 	// t := conn.(*net.TCPConn)
@@ -51,17 +59,21 @@ func transmit() {
 	defer conn.Close()
 
 	start := time.Now()
+
+	//发送啥？？
 	err = binary.Write(conn, binary.BigEndian, &sessionMessage)
 	muduo.PanicOnError(err)
 
 	total_len := 4 + opt.length // binary.Size(int32(0)) == 4
 	// println(total_len)
+
 	payload := make([]byte, total_len)
 	binary.BigEndian.PutUint32(payload, uint32(opt.length))
 	for i := 0; i < opt.length; i++ {
 		payload[4+i] = "0123456789ABCDEF"[i%16]
 	}
 
+	//多少个number个数
 	for i := 0; i < opt.number; i++ {
 		var n int
 		n, err = conn.Write(payload)
@@ -78,10 +90,17 @@ func transmit() {
 	fmt.Printf("%.3f seconds\n%.3f MiB/s\n", elapsed, total_mb/elapsed)
 }
 
+//服务器端 接受
 func receive() {
+
+	//服务器端 监听 !
 	listener := muduo.ListenTcpOrDie(fmt.Sprintf(":%d", opt.port))
+
+	//两种资源 一个是 监听器！
 	defer listener.Close()
 	println("Accepting", listener.Addr().String())
+
+	//一个是连接器 （另外一种资源！）
 	conn, err := listener.Accept()
 	muduo.PanicOnError(err)
 	defer conn.Close()
@@ -91,15 +110,19 @@ func receive() {
 	err = binary.Read(conn, binary.BigEndian, &sessionMessage)
 	muduo.PanicOnError(err)
 
-	fmt.Printf("receive buffer length = %d\nreceive number of buffers = %d\n",
+	fmt.Printf("receive buffer length = %d\n receive number of buffers = %d\n",
 		sessionMessage.Length, sessionMessage.Number)
+
 	total_mb := float64(sessionMessage.Number) * float64(sessionMessage.Length) / 1024.0 / 1024.0
 	fmt.Printf("%.3f MiB in total\n", total_mb)
 
+	// 消息体:
 	payload := make([]byte, sessionMessage.Length)
 	start := time.Now()
 	for i := 0; i < int(sessionMessage.Number); i++ {
+
 		var length int32
+		// 先读长度
 		err = binary.Read(conn, binary.BigEndian, &length)
 		muduo.PanicOnError(err)
 		muduo.Check(length == sessionMessage.Length, "read length")
@@ -118,18 +141,22 @@ func receive() {
 	fmt.Printf("%.3f seconds\n%.3f MiB/s\n", elapsed, total_mb/elapsed)
 }
 
+//client ./ttcp -host  127.0.0.1
+//server ./ttcp -r
+
 func main() {
 	flag.Parse()
 	opt.transmit = opt.host != ""
+
 	if opt.transmit == opt.receive {
-		println("Either -r or -t must be specified.")
+		println("Either -r or -host must be specified.")
 		return
 	}
 
 	if opt.transmit {
-		transmit()
+		transmit() //发送
 	} else if opt.receive {
-		receive()
+		receive() //接受
 	} else {
 		panic("unknown")
 	}
