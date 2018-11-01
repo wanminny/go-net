97ca1bbb97a3c1119fc55686de54c0afe1bdf8cc
wanmin <wanminny@163.com>
2018-11-01 15:41:09 +0800

    注释

--- examples/asio/chat/asio_chat_server/main.go
+++ examples/asio/chat/asio_chat_server/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/chenshuo/muduo-examples-in-go/examples/asio/chat"
+	"gobible/muduo-go/examples/asio/chat"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
-
 	server := chat.NewChatServer(":3399")
 	server.Run()
 }
--- examples/asio/chat/server.go
+++ examples/asio/chat/server.go
@@ -9,22 +9,25 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/chenshuo/muduo-examples-in-go/muduo"
+	"gobible/muduo-go/muduo"
 )
 
 type ChatServer struct {
-	listener   net.Listener
-	conns      map[*connection]bool
-	boardcast  chan []byte
-	register   chan *connection
-	unregister chan *connection
+	listener   net.Listener  //每个server都有一个监听listener
+
+	conns      map[*connection]bool //所有连接
+	register   chan *connection  // 注册
+	unregister chan *connection  //注销
+
+	broadcast  chan []byte  //要广播的消息
 }
 
+// 新建 server 【全部初始化 map chan etc.】
 func NewChatServer(listenAddr string) *ChatServer {
 	return &ChatServer{
 		listener:   muduo.ListenTcpOrDie(listenAddr),
 		conns:      make(map[*connection]bool),
-		boardcast:  make(chan []byte),      // size?
+		broadcast:  make(chan []byte),      // size?
 		register:   make(chan *connection), // size?
 		unregister: make(chan *connection), // size?
 	}
@@ -36,31 +39,37 @@ type connection struct {
 	send chan []byte
 }
 
-func (c *connection) input(boardcast chan []byte) {
+func (c *connection) input(broadcast chan []byte) {
 	for {
 		message, err := c.readMessage()
 		if err != nil {
 			log.Println(err)
 			break
 		}
-		boardcast <- message
+		broadcast <- message
 	}
 }
 
 func (c *connection) output() {
 	defer c.close()
 	for m := range c.send {
+
+		//先发送长度 【1】
 		err := binary.Write(c.conn, binary.BigEndian, int32(len(m)))
 		if err != nil {
 			log.Println(err)
 			break
 		}
 		var n int
+		//后发生内容 【2】
+		//TODO 【1】【2】区别？
+
 		n, err = c.conn.Write(m)
 		if err != nil {
 			log.Println(err)
 			break
 		}
+		// 判断发生的内容是否正确
 		if n != len(m) {
 			log.Println("short write")
 			break
@@ -96,13 +105,14 @@ func (c *connection) readMessage() (message []byte, err error) {
 	return message, nil
 }
 
+// 连接 协程 【区别于主协程】
 func (s *ChatServer) ServeConn(conn net.Conn) {
 	c := &connection{conn: conn, send: make(chan []byte, 1024)}
 	s.register <- c
 	defer func() { s.unregister <- c }()
 
 	go c.output()
-	c.input(s.boardcast)
+	c.input(s.broadcast)
 }
 
 func (s *ChatServer) Run() {
@@ -110,12 +120,17 @@ func (s *ChatServer) Run() {
 	go muduo.ServeTcp(s.listener, s, "chat")
 	for {
 		select {
-		case c := <-s.register:
+
+		case c := <-s.register: //注册
+			//所有连接
 			s.conns[c] = true
-		case c := <-s.unregister:
+
+		case c := <-s.unregister:  // 注销
+			//
 			delete(s.conns, c)
 			close(c.send)
-		case m := <-s.boardcast:
+
+		case m := <-s.broadcast:  //广播
 			for c := range s.conns {
 				select {
 				case c.send <- m:
